837145c141d8228f5dec11af18da0f8a8c42d9d9
Todd Guse <todd.guse@9thcell.com>
2013-02-13 14:46:19 -0600

    First Commit
    Used to hash password to a text file using sha512

---
+++ passhash512.py
@@ -0,0 +1,29 @@
+#python
+
+import sys
+import hashlib
+import getpass
+
+def main(argv):
+
+	if len(argv) != 1:
+		sys.exit('Usage: passhash512.py <file_name>')
+
+	print '\nPassword Storage Program v.01\n'
+	
+	if raw_input('The file ' + sys.argv[1] + ' will be erased or overwrite if existing.\nDo you wish to continue (Y/n): ') not in ('Y','y') :
+		sys.exit('\nChanges were not recorded\n')
+	
+	password = hashlib.sha512(getpass.getpass('Please Enter a Password: ')).hexdigest()
+
+	try:
+		file_conn = open(sys.argv[1],'w')
+		file_conn.write(password + '\n')
+		file_conn.close()
+	except:
+		sys.exit('There was a problem writing to the file!')
+
+	print '\nPassword safely stored in ' + sys.argv[1] + '\n'		
+			
+if __name__ == "__main__":
+	main(sys.argv[1:])
